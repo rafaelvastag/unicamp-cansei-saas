986d6797c10cd9b3b574641a6dc57664d7465a43
Rafael Vastag <rgvastag@gmail.com>
2022-10-05 08:28:17 -0300

    initial

---
+++ src/main/java/com/unicamp/inf332/cansei/CanseiSaasApplication.java
@@ -0,0 +1,13 @@
+package com.unicamp.inf332.cansei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanseiSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanseiSaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicamp/inf332/cansei/CanseiSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicamp.inf332.cansei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanseiSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
